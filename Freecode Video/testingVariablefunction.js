99dfc2ba8496172ff868b0b9721c2c03b366336d
Nathaniel <dehinbonatt@gmail.com>
2023-01-14 22:32:01 +0100

    JavaScript Learning

---
+++ Freecode Video/testingVariablefunction.js
@@ -0,0 +1,14 @@
+var a = 44 / 4;
+a *= 13
+// a = 1011;
+console.log(a);
+// Hello
+var test = "Testing string and double quotes \"Jesus is Lord\" and yes he's Lord";
+console.log(test);
+var test1 = 'Testing string and double quotes "Jesus is Lord" and yes he is Lord';
+console.log(test);
+var concatenatestring = "I come first " + "I aint Second"
+console.log(concatenatestring);
+var concatenatestring2 = "I come first "; 
+concatenatestring2 += "I aint Second"
+console.log(concatenatestring);
