08d04c6d9e9df31cf08618d03295ec58ddb94a62
Nathaniel <dehinbonatt@gmail.com>
2023-03-12 01:00:22 +0100

    adding global file

--- NodeClasses/global.js
+++ NodeClasses/global.js
@@ -4,4 +4,9 @@
 
 setTimeout(() => {
     console.log(`God is the Greatest`)
-}, 3000);
+    clearInterval(int);
+}, 30000);
+
+const int = setInterval(() => {
+    console.log(`Jesus is King`);
+}, 1000);
