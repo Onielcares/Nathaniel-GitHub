060eb4edd41d976c0f9bd4ea22ffcf317ca5c162
Nathaniel <dehinbonatt@gmail.com>
2023-06-17 17:11:40 +0100

    practice

---
+++ Udemy Javascript Course/2nd/variable.js
@@ -0,0 +1,14 @@
+var x = 25;
+var x = 100;
+x = 40;
+document.write(x)
+// Declare a variable a number of times
+
+let y = 50;
+y = 40;
+y = 30;
+// Declare a variable once and value once
+
+const z = 100;
+z = 200;
+// Declare a variable once and no other value
