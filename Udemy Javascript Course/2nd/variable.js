866955210125d2659d9269aaacf156135b411266
Nathaniel <dehinbonatt@gmail.com>
2023-06-19 10:39:05 +0100

    statement conditions

--- Udemy Javascript Course/2nd/variable.js
+++ Udemy Javascript Course/2nd/variable.js
@@ -1,20 +1,49 @@
-var x = "100";
-// var x = "100";
-function fun(params) {
-    document.write(x)
+// // var x = "100";
+// // // var x = "100";
+// // function fun(params) {
+// //     document.write(x)
+// // }
+// // var x = "25";
+// // document.write(x)
+
+// // // Declare a variable a number of times
+
+// // let y = "50";
+// // y = "40";
+// // y = "30";
+// // document.write(y)
+// // // Declare a variable once and value once
+
+// // const z = "100";
+// // z = "200";
+// // document.write(z)
+// // // Declare a variable once and no other value
+
+// // STATEMENTS
+
+// // IF Statements
+
+a = 10;
+b = 20;
+if (a < b) {
+    document.write("a is less than b")
 }
-var x = "25";
-document.write(x)
 
-// Declare a variable a number of times
+// // IF ELSE Statements
 
-let y = "50";
-y = "40";
-y = "30";
-document.write(y)
-// Declare a variable once and value once
+a = 10;
+b = 20;
+if (a > b) {
+    document.write("a is greater than b")
+} else {
+    document.write("a is lesser than b")
+}
 
-const z = "100";
-z = "200";
-document.write(z)
-// Declare a variable once and no other value
+// CONDITION TENARY OPERATOR
+
+var x = 100;
+var y;
+// y = (x > 50) ? "x is greater than 50" : "x is lesser than 50";
+x == 100 ? (y = "Yes") : (y = "No");
+document.write(y);
+
