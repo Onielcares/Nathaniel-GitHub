bb4c9674bef85a438778187b6171f3bf57a982c9
Nathaniel <dehinbonatt@gmail.com>
2024-04-16 18:14:03 +0100

    Adding Factorial and Primality

--- Algo_DSA/Big_O_Notation.js
+++ Algo_DSA/Big_O_Notation.js
@@ -8,6 +8,8 @@ function summation(n) {
 
 console.log(summation(5))
 
+// Big O Notation. Math Algorithm. 1st Maths Algorithm.
+
 function fibonacci(n) {
     const fib = [0, 1]
     for (let i = 2; i < n; i++) {
@@ -25,3 +27,45 @@ console.log(fibonacci(7))
 console.log(fibonacci(8))
 console.log(fibonacci(9))
 console.log(fibonacci(10))
+
+n = [0, 1]
+m = [0, 1, 1]
+d = n + m
+console.log(d)
+
+// Big O Notation.
+// O(n) - Linear Time Complexity. Line 14 is for loop hence O(n).
+
+// Big O Notation. 2nd Maths Algorithm. Factorial of a number.
+
+function factorial(n) {
+    let fact = 1
+    for (let i = 2; i <= n; i++) {
+        fact *= i
+    }
+    return fact
+}
+
+console.log(factorial(1))
+console.log(factorial(2))
+console.log(factorial(3))
+console.log(factorial(4))
+console.log(factorial(5))
+console.log(factorial(6))
+console.log(factorial(7))
+
+// Big O Notation.
+// O(n) - Linear Time Complexity. Line 43 is for loop hence O(n).
+
+// Big O Notation. 3rd Maths Algorithm. Prime Number.
+
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isPrime(100))
