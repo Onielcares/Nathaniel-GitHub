00ce719de74ac9f634bd1524330dbdd2e82523b0
Nathaniel <dehinbonatt@gmail.com>
2024-04-24 14:47:14 +0100

    updating

--- Algo_DSA/Search_algo.js
+++ Algo_DSA/Search_algo.js
@@ -14,3 +14,11 @@ function linearSearch(arr, val) {
 console.log(linearSearch([1,2,3,4,5,6,7,8,9,10], 5))
 console.log(linearSearch([1,2,3,4,5,6,7,8,9,10], 10))
 console.log(linearSearch([1,2,3,4,5,6,7,8,9,10], 15))
+
+// complexity is O(n) - Line 6 is a loop hence O(n)
+
+// Binary Search
+
+//  Binary search works on a sorted array
+
+
