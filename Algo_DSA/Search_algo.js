4e25376286ec5dc0054330a7f6db5da8ac319463
Nathaniel <dehinbonatt@gmail.com>
2024-04-25 03:59:35 +0100

    updating

--- Algo_DSA/Search_algo.js
+++ Algo_DSA/Search_algo.js
@@ -19,6 +19,26 @@ console.log(linearSearch([1,2,3,4,5,6,7,8,9,10], 15))
 
 // Binary Search
 
-//  Binary search works on a sorted array
+//  Binary search only works on a sorted array
+// You have to find the Middle Element to get the index it serves as a comparism
 
+function binarySearch(arr, val) {
+    let start = 0
+    let end = arr.length - 1
+    let middle = Math.floor((start + end) / 2)
+
+    while (arr[middle] !== val && start <= end) {
+        if (val < arr[middle]) {
+            end = middle - 1
+        } else {
+            start = middle + 1
+        }
+        middle = Math.floor((start + end) / 2)
+    }
+    return arr[middle] === val ? middle : -1
+}
+
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 5))
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 10))
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 15))
 
