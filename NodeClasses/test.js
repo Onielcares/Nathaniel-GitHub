bf46c7d17567171ba517eb7ef57942979790ecf1
Nathaniel <dehinbonatt@gmail.com>
2024-05-09 17:24:40 +0100

    testing math random for otp

--- NodeClasses/test.js
+++ NodeClasses/test.js
@@ -3,3 +3,25 @@ let number1 = 5;
 let number2 = 10;
 let result = number1 + number2;
 console.log(result);
+
+function generateOTP() {
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    return otp.toString();
+}
+
+console.log("Generated OTP:", generateOTP());
+
+
+function generateOTP(length = 6) {
+    const chars = '0123456789';
+    let otp = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      otp += chars[randomIndex];
+    }
+    return otp;
+}
+
+const otp = generateOTP()
+
+console.log(otp)
