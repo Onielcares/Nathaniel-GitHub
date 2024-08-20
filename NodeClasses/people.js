c56ab03e3b621e848602fea2fc575c70ea8f8aa7
Nathaniel <dehinbonatt@gmail.com>
2024-08-20 17:57:12 +0100

    testing

--- NodeClasses/people.js
+++ NodeClasses/people.js
@@ -1,1 +1,12 @@
 const people = ['Bola', 'Funmi', 'Seun', 'Mope', 'Nihin']
+console.log(people.length)
+//
+for (let person of people) {
+    console.log(person);
+}
+//
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers.length);
+for (let number of numbers) {
+    console.log(number);
+}
